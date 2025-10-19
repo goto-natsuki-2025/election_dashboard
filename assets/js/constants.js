dc46092172972e2d76837eefa9f2cbbdf5adaf04
goto-natsuki-2025 <goto-natsuki-2025@users.noreply.github.com>
2025-10-19 12:36:35 +0000

    docs: deploy production site 1f0f0aa2a5620d069a28108d4a9e14f2223b7fea

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -3,6 +3,13 @@ export const DATA_PATH = {
   candidates: "data/candidate_details.csv.gz",
 };
 
-export const WINNING_KEYWORDS = ["当選", "補欠当選", "繰上当選", "当せん", "再選"];
+export const WINNING_KEYWORDS = [
+  "\u5f53\u9078",
+  "\u88dc\u6b20\u5f53\u9078",
+  "\u7e70\u4e0a\u5f53\u9078",
+  "\u7e70\u308a\u4e0a\u3052\u5f53\u9078",
+  "\u5f53\u305b\u3093",
+  "\u518d\u9078",
+];
 
 export const TERM_YEARS = 4;
