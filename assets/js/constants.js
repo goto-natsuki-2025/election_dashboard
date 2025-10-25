c0c321e04859d8bfb9d4443ca45b3e3f4c14665f
goto-natsuki-2025 <goto-natsuki-2025@users.noreply.github.com>
2025-10-25 03:35:28 +0000

    docs: deploy production site 535991567b0dc891b59ccb621dbd3085479aa5c7

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,15 +1,20 @@
-export const DATA_PATH = {
+﻿export const DATA_PATH = {
   elections: "data/election_summary.csv",
   candidates: "data/candidate_details.csv.gz",
 };
 
 export const WINNING_KEYWORDS = [
-  "\u5f53\u9078",
-  "\u88dc\u6b20\u5f53\u9078",
-  "\u7e70\u4e0a\u5f53\u9078",
-  "\u7e70\u308a\u4e0a\u3052\u5f53\u9078",
-  "\u5f53\u305b\u3093",
-  "\u518d\u9078",
+  "当選",
+  "補欠当選",
+  "繰上当選",
+  "繰り上げ当選",
+  "当せん",
+  "再選",
 ];
 
 export const TERM_YEARS = 4;
+
+export const PARTY_FOUNDATION_DATES = {
+  "立憲民主党": "2020-09-15",
+  "日本維新の会": "2015-11-02",
+};
