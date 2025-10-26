afab16bb1db07328d6559b68d24a87f00b011016
goto-natsuki-2025 <goto-natsuki-2025@users.noreply.github.com>
2025-10-26 00:44:25 +0000

    docs: deploy preview for PR #25 90f0b3150afbc009ed43681968d46cec48804974

---
+++ pr-preview/pr-25/assets/js/constants.js
@@ -0,0 +1,20 @@
+﻿export const DATA_PATH = {
+  elections: "data/election_summary.csv",
+  candidates: "data/candidate_details.csv.gz",
+};
+
+export const WINNING_KEYWORDS = [
+  "当選",
+  "補欠当選",
+  "繰上当選",
+  "繰り上げ当選",
+  "当せん",
+  "再選",
+];
+
+export const TERM_YEARS = 4;
+
+export const PARTY_FOUNDATION_DATES = {
+  "立憲民主党": "2020-09-15",
+  "日本維新の会": "2015-11-02",
+};
